21f175613f5df5c79de89ae7627f1b74c8ac7473
medipodo <medipodotr@gmail.com>
2026-04-27 13:45:17 +0300

    Update App.js
    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -45,6 +45,7 @@ import AyakSagligiRehberiBlog from './pages/AyakSagligiRehberiBlog';
 import PodologFiyatlariAnkaraBlog from './pages/PodologFiyatlariAnkaraBlog';
 import YazlikAyakkabiSecimiBlog from './pages/YazlikAyakkabiSecimiBlog';
 import TirnakTeliFiyatlariBlog from './pages/TirnakTeliFiyatlariBlog';
+import AnkaraPodolojiHastanelerBlog from './pages/AnkaraPodolojiHastanelerBlog';
 import AyakAnalizi from './pages/AyakAnalizi';
 import PrivacyPolicy from './pages/PrivacyPolicy';
 import TermsOfService from './pages/TermsOfService';
@@ -108,6 +109,7 @@ function AppContent() {
         <Route path="/blog/podolog-fiyatlari-ankara" element={<PodologFiyatlariAnkaraBlog />} />
         <Route path="/blog/yaz-aylarinda-ayakkabi-secimi" element={<YazlikAyakkabiSecimiBlog />} />
         <Route path="/blog/tirnak-teli-fiyatlari-ankara" element={<TirnakTeliFiyatlariBlog />} />
+        <Route path="/blog/ankara-podoloji-bolumu-olan-hastaneler" element={<AnkaraPodolojiHastanelerBlog />} />
         <Route path="/blog/:slug" element={<BlogDetail />} />
         <Route path="/hakkimizda" element={<About />} />
         <Route path="/kadromuz" element={<Team />} />
